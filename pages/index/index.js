59fedc691c836a9ca3f316335f5721e048c0c57d
Jasper Hou <jasper.hou@bmw.com>
2020-03-11 00:48:16 +0800

    add the new timer view

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,243 +1,306 @@
 //index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
     width: 0,
     height: 0,
-    showMask: false
-
+    angle: 0,
+    leftMin: 25,
+    timerTriggered: 0
   },
-  //事件处理函数
+  
   addTimer: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-    var that = this
-    //获取系统信息  
+    var that = this;
     wx.getSystemInfo({
-      //获取系统信息成功，将系统窗口的宽高赋给页面的宽高  
       success: function (res) {
         that.width = res.windowWidth
         that.height = res.windowHeight
-        that.setData({
-          canvasWidth: res.windowWidth,
-          canvasHeight: res.windowWidth * 0.9 * 0.90 * 0.9819,
-          rightWidth: res.windowWidth * 0.9 * 0.80
-        })
-      }
+      } 
     })
   },
   onReady: function () {
-    this.drawClock();
-    // 每40ms执行一次drawClock()，
-    this.interval = setInterval(this.drawClock, 40);
+    this.drawClock()
+    //this.interval = setInterval(this.drawClock, 50);
   },
-  // 所有的canvas属性以及Math.sin,Math.cos()等涉及角度的参数都是用弧度表示
-  // 时钟
-  drawClock: function () {
-    let _this = this;
-    const ctx = wx.createCanvasContext('clock');
-    var height = this.height;
-    var width = this.width;
-    // 设置文字对应的半径
-    var R = this.data.canvasWidth / 5;
-    ctx.save();
-    // 把原点的位置移动到屏幕中间，及宽的一半，高的一半
-    ctx.translate(this.data.canvasWidth / 2, this.data.canvasHeight / 2);
+  drawClock: function() {
+    let that = this;
+    that.setData({
+      timerTriggered: (that.data.timerTriggered + 1) % 1200
+    });
 
-    // 画外框
-    function drawBackground() {
-      ctx.setStrokeStyle('#ff6347');
-      // 设置线条的粗细，单位px
-      ctx.setLineWidth(8);
-      // 开始路径
-      ctx.beginPath();
-      // 运动一个圆的路径
-      // arc(x,y,半径,起始位置，结束位置，false为顺时针运动)
-      ctx.arc(0, 0, R * 1.6, 0, 2 * Math.PI, false);
-      ctx.closePath();
-      // 描出点的路径
-      ctx.stroke();
-    };
-    // 画时钟数
-    function drawHoursNum() {
-      ctx.setFontSize(20);
-      // 圆的起始位置是从3开始的，所以我们从3开始填充数字
-      var hours = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
-      hours.forEach(function (hour, i) {
-        var rad = (2 * Math.PI / 12) * i;
-        var x = R * Math.cos(rad);
-        var y = R * Math.sin(rad);
-        if (hour == 12) {
-          ctx.fillText(hour, x - 11, y - 3);
-        } else if (hour == 6) {
-          ctx.fillText(hour, x - 5, y + 20);
-        } else if (hour == 3) {
-          ctx.fillText(hour, x + 8, y + 8);
-        } else if (hour == 9) {
-          ctx.fillText(hour, x - 20, y + 8);
-        }
-        else {
-          //ctx.fillText(hour, x - 6, y + 6);
-        }
-      })
-    };
+    var context = wx.createContext()//创建并返回绘图上下文（获取画笔）
+    //设置宽高
+    var width = this.width
+    var height = this.height
+    var R = width / 2 - 55;//设置文字距离时钟中心点距离
 
-    // 画数字对应的点
-    function drawdots() {
-      for (let i = 0; i < 60; i++) {
-        var rad = 2 * Math.PI / 60 * i;
-        var x = (R + 30) * Math.cos(rad);
-        var y = (R + 30) * Math.sin(rad);
-        ctx.beginPath();
-        // 每5个点一个比较大
-        if (i % 5 == 0) {
-          ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+    that.setData({
+      width: width,
+      height: height
+    });
+    //重置画布函数
+    function reset() {
+      context.draw();
+      context.height = context.height;//每次清除画布，然后变化后的时间补上
+      context.translate(width / 2, height / 2 - 70);//设置坐标轴原点
+      context.save();//保存中点坐标1
+    }
+    //绘制中心圆和外面大圆
+    function circle() {
+      //外面大圆
+      context.setLineWidth(5);
+      context.beginPath();
+      context.arc(0, 0, width / 2 - 30, 0, 2 * Math.PI, true);
+      context.closePath();
+      context.stroke();
+      //外圆内圈
+      context.setLineWidth(2);
+      context.beginPath();
+      context.arc(0, 0, width / 2 - 44, 0, 2 * Math.PI, true);
+      context.closePath();
+      context.stroke();
+
+      //中心圆
+      context.setLineWidth(4);
+      context.beginPath();
+      context.arc(0, 0, width / 2 - 110, 0, 2 * Math.PI, true);
+      context.closePath();
+      context.stroke();
+
+      //ears
+      context.setLineWidth(5);
+      context.beginPath();
+      context.arc(0, 0, width / 2 - 10, -1.93, -1.22, true);
+      //context.closePath();
+      context.stroke();
+
+      context.setLineWidth(5);
+      context.beginPath();
+      context.arc(0, 0, width / 2 + 5, -1.80, -1.35, false);
+      context.stroke();
+
+      context.setLineWidth(7);
+      context.beginPath();
+      context.arc(0, 0, width / 2 + 12, -1.80, -1.35, false);
+      context.stroke();
+
+      context.setLineWidth(4);
+      context.beginPath();
+      context.arc(-93, -159, 30, -0.5, 2.6, true);
+      context.closePath();
+      context.stroke();
+
+      context.setLineWidth(4);
+      context.beginPath();
+      context.arc(-110, -189, 1, -0.5, 2.6, true);
+      context.closePath();
+      context.stroke();
+
+      context.setLineWidth(4);
+      context.beginPath();
+      context.arc(93, -158, 30, 0.5, -2.6, true);
+      context.closePath();
+      context.stroke();
+
+      context.setLineWidth(4);
+      context.beginPath();
+      context.arc(110, -188, 1, 0.5, -2.6, true);
+      context.closePath();
+      context.stroke();
+
+    }
+    //绘制字体
+    function num() {
+      // var R = width/2-60;//设置文字距离时钟中心点距离
+      context.setFontSize(20)//设置字体样式
+      context.textBaseline = "middle";//字体上下居中，绘制时间
+      for (var i = 1; i < 13; i++) {
+        //利用三角函数计算字体坐标表达式
+        var x = R * Math.cos(i * Math.PI / 6 - Math.PI / 2);
+        var y = R * Math.sin(i * Math.PI / 6 - Math.PI / 2);
+        if (i == 11 || i == 12) {//调整数字11和12的位置
+          context.fillText(i * 5, x - 12, y + 2);
         } else {
-          // ctx.arc(x, y, 1, 0, 2 * Math.PI, false);
+          context.fillText(i * 5, x - 10, y);
         }
-        ctx.setFillStyle('black');
-        ctx.fill();
       }
-      ctx.closePath();
     }
-
-    // 画时针
-    function drawHour(hour, minute) {
-      ctx.setStrokeStyle('#000000');
-      // 保存画之前的状态
-      ctx.save();
-      ctx.beginPath();
-      // 根据小时数确定大的偏移
-      var rad = 2 * Math.PI / 12 * hour;
-      // 根据分钟数确定小的偏移
-      var mrad = 2 * Math.PI / 12 / 60 * minute;
-      // 做旋转
-      ctx.rotate(rad + mrad);
-      ctx.setLineWidth(4);
-      // 设置线条结束样式为圆
-      ctx.setLineCap('round');
-      // 时针向后延伸8个px；
-      ctx.moveTo(0, 8);
-      // 一开始的位置指向12点的方向，长度为R/2
-      ctx.lineTo(0, -R / 2);
-      ctx.stroke();
-      ctx.closePath();
-      // 返回画之前的状态
-      ctx.restore();
-    }
-
-    // 画分针
-    function drawMinute(minute, second) {
-      ctx.save();
-      ctx.beginPath();
-      // 根据分钟数确定大的偏移
-      var rad = 2 * Math.PI / 60 * minute;
-      // 根据秒数确定小的偏移
-      var mrad = 2 * Math.PI / 60 / 60 * second;
-      ctx.rotate(rad + mrad);
-      // 分针比时针细
-      ctx.setLineWidth(3);
-      ctx.setLineCap('round');
-      ctx.moveTo(0, 10);
-      // 一开始的位置指向12点的方向，长度为3 * R / 4
-      ctx.lineTo(0, -3 * R / 4);
-      ctx.stroke();
-      ctx.closePath();
-      ctx.restore();
-    }
-
-    // 画秒针
-    function drawSecond(second, msecond) {
-
-      ctx.save();
-      ctx.beginPath();
-      // 根据秒数确定大的偏移
-      var rad = 2 * Math.PI / 60 * second;
-      // 1000ms=1s所以这里多除个1000
-      var mrad = 2 * Math.PI / 60 / 1000 * msecond;
-      ctx.rotate(rad + mrad);
-      ctx.setLineWidth(2);
-      ctx.setStrokeStyle('#ff6347');
-      ctx.setLineCap('round');
-      ctx.moveTo(0, 12);
-      ctx.lineTo(0, -R);
-      ctx.stroke();
-      ctx.closePath();
-      ctx.restore();
-    }
-
-    //画出中间那个灰色的圆
-    function drawDot() {
-      ctx.beginPath();
-      ctx.arc(0, 0, 4, 0, 2 * Math.PI, false);
-      ctx.setFillStyle('#FFF9E6');
-      ctx.setLineWidth(6);
-      ctx.setStrokeStyle('#000000');
-      ctx.stroke();
-      ctx.fill();
-      ctx.closePath();
-    }
-    //画蒙层
-    function drawMask() {
-      ctx.restore();
-      ctx.rect(0, 0, width * 0.5, _this.data.canvasHeight);
-      ctx.setFillStyle('rgba(0,0,0,.2)')
-      ctx.fill()
-    }
-    function Clock() {
-      // 实时获取各个参数
-      var now = new Date();
-      var hour = now.getHours();
-      var minute = now.getMinutes()
-      var second = now.getSeconds();
-      var msecond = now.getMilliseconds();
-      if (_this.data.showMask) {
-        ctx.scale(0.98, 0.98)
+    //绘制小格
+    function smallGrid(context) {
+      context.setLineWidth(1);
+      context.rotate(-Math.PI / 2);//时间从3点开始，倒转90度
+      for (var i = 0; i < 60; i++) {
+        context.beginPath();
+        context.rotate(Math.PI / 30);
+        context.moveTo(width / 2 - 30, 0);
+        context.lineTo(width / 2 - 40, 0);
+        context.stroke();
       }
-      // 依次执行各个方法
-      drawBackground();
-      drawHoursNum();
-      drawdots();
-      drawSecond(second, msecond);
-      drawHour(hour, minute);
-      drawMinute(minute, second);
-      drawDot();
-      if (_this.data.showMask) {
-        drawMask();
-      }
-      ctx.draw();
     }
-    Clock();
-  },
-  goCountdown() {
-    let _this = this;
-    _this.setData({
-      showMask: true
-    })
-    setTimeout(function () {
-      _this.setData({
-        showMask: false
-      })
-      wx.navigateTo({
-        url: '/pages/countdown/countdown',
-      })
-    }, 200)
+    //绘制大格
+    function bigGrid(context) {
+      for (var i = 0; i < 12; i++) {
+        context.beginPath();
+        context.rotate(Math.PI / 6);
+        if (i == 10 || i == 0 || i == 11) {
+          context.setLineWidth(10);
+          context.moveTo(width / 2 - 10, 0);
+          context.lineTo(width / 2 - 25, 0);
+          context.stroke();
 
-  },
-  touchstart: function (e) {
-    console.log(e)
-    this.setData({
-      showMask: true
+          if (i == 11) {
+            context.setLineWidth(10);
+            context.moveTo(width / 2 - 8, 0);
+            context.lineTo(width / 2 + 5, 0);
+            context.stroke();
+          }
+        } 
+        
+        context.setLineWidth(5);
+        context.moveTo(width / 2 - 30, 0);
+        context.lineTo(width / 2 - 45, 0);
+        context.stroke();
+      }
+    }
+    //指针运动函数
+    function move() {
+      var t = new Date();//获取当前时间
+      var h = t.getHours();//获取小时
+      h = h > 12 ? (h - 12) : h;//将24小时制转化为12小时制
+      var m = t.getMinutes();//获取分针
+      var s = t.getSeconds();//获取秒针
+      context.save();//再次保存2
+      //context.setLineWidth(7);
+      //旋转角度=30度*（h+m/60+s/3600）
+      //分针旋转角度=6度*（m+s/60）
+      //秒针旋转角度=6度*s
+      //context.beginPath();
+      
+      //绘制秒针
+      context.setStrokeStyle('#ff6347');
+      context.setLineWidth(15);
+      context.beginPath();
+      context.rotate((Math.PI / 30) * s);
+      context.moveTo(142, 0);
+      context.lineTo(width / 3 + 32, 0);
+      context.closePath();
+      context.stroke();
+    }
+    function drawProgress() {
+      var contextProgress = wx.createCanvasContext('canvasProgress', this);
+      contextProgress.translate(width / 2, height / 2 - 70);//设置坐标轴原点
+      contextProgress.height = that.data.height;//每次清除画布，然后变化后的时间补上
+      contextProgress.width = that.data.width;
+      contextProgress.save();
+
+      contextProgress.rotate(-Math.PI / 2);
+
+      contextProgress.setLineWidth(12);
+      contextProgress.setStrokeStyle("#AAAAAA");
+      contextProgress.beginPath();
+      // 参数step 为绘制的圆环周长，从0到2为一周 。 -Math.PI / 2 将起始角设在12点钟位置 ，结束角 通过改变 step 的值确定
+      contextProgress.arc(0, 0, that.data.width / 2 - 38, 0, that.data.angle, true);
+      contextProgress.stroke();
+      contextProgress.draw();
+
+      that.setData({
+        angle: (Math.PI / 600 + that.data.angle) % (2 * Math.PI)
+      });
+
+      //console.log(that.data.angle);
+    }
+    function drawLines() {
+      var contextProgress = wx.createCanvasContext('canvasLine', this);
+      contextProgress.translate(that.data.width / 2, that.data.height / 2 - 70);//设置坐标轴原点
+      contextProgress.height = that.data.height;//每次清除画布，然后变化后的时间补上
+      contextProgress.width = that.data.width;
+      contextProgress.save();
+      contextProgress.setLineWidth(1);
+      contextProgress.rotate(-Math.PI / 2);//时间从3点开始，倒转90度
+      for (var i = 0; i < 60; i++) {
+        contextProgress.beginPath();
+        contextProgress.rotate(Math.PI / 30);
+        contextProgress.moveTo(that.data.width / 2 - 30, 0);
+        contextProgress.lineTo(that.data.width / 2 - 40, 0);
+        contextProgress.stroke();
+      }
+
+      for (var i = 0; i < 12; i++) {
+        contextProgress.beginPath();
+        contextProgress.rotate(Math.PI / 6);
+        contextProgress.setLineWidth(5);
+        contextProgress.moveTo(that.data.width / 2 - 30, 0);
+        contextProgress.lineTo(that.data.width / 2 - 45, 0);
+        contextProgress.stroke();
+      }
+
+      contextProgress.draw();
+    }
+    function showCountDownTime() {
+      if (that.data.timerTriggered == 0) {
+        let left = that.data.leftMin - 1;
+        that.setData({
+            leftMin: left
+        });
+      }
+
+      context.setFontSize(90)//设置字体样式
+      context.textBaseline = "middle";//字体上下居中，绘制时间
+      context.setFillStyle('#2F4F4F');
+      context.fillText(that.data.leftMin, -context.measureText(that.data.leftMin).width * 0.5, -10);
+      context.setFillStyle('#A9A9A9');
+      context.setFontSize(20)//设置字体样式
+      context.textBaseline = "middle";//字体上下居中，绘制时间
+      context.fillText("MIN", -15, 50);
+      //console.log(width - context.measureText(that.data.leftMin).width);
+    }
+    //调用
+    function drawClock() {
+      reset();
+      circle();
+      num();
+      showCountDownTime();
+      smallGrid(context);
+      bigGrid(context);
+    }
+    drawClock()//调用运动函数
+    drawProgress();
+    drawLines();
+
+    if (that.data.leftMin == 0) {
+      clearInterval(that.interval);
+    }
+    // 调用 wx.drawCanvas，通过 canvasId 指定在哪张画布上绘制，通过 actions 指定绘制行为
+    wx.drawCanvas({
+      canvasId: 'clock',
+      actions: context.getActions()
     })
   },
-  touchend: function (e) {
-    this.setData({
-      showMask: false
-    })
+  onUnload: function () {
+    clearInterval(this.interval);
+  },
+
+  toggleTimer: function() {
+    if (!this.interval) {
+      this.interval = setInterval(this.drawClock, 50);
+    } else {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
   }
+
+  //bussiness part
+
+
+
+
+
+
+
 })
